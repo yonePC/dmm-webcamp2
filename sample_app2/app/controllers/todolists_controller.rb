f99835b598fc4d9b43962b69397925babc3154c1
yonePC <ec2-user@ip-172-31-13-107.ec2.internal>
2021-06-11 08:56:40 +0000

    Rails2-6

--- sample_app2/app/controllers/todolists_controller.rb
+++ sample_app2/app/controllers/todolists_controller.rb
@@ -2,32 +2,41 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # ３. トップ画面へリダイレクト
-    
-    redirect_to todolist_path(list.id)
 
+    redirect_to todolist_path(list.id)
   end
-    
+
   def index
     @lists=List.all
   end
-    
+
   def show
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
 
   private
-  
+
   def list_params
     params.require(:list).permit(:title,:body)
   end
-  
-  
+
+
 end

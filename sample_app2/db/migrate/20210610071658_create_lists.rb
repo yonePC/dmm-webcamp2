4821a358433e6e3e55adf774949969d440aa5a24
yonePC <ec2-user@ip-172-31-13-107.ec2.internal>
2021-06-11 08:29:09 +0000

    Rails5-2-2

--- sample_app2/db/migrate/20210610071658_create_lists.rb
+++ sample_app2/db/migrate/20210610071658_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :title
-      t.string :body  
+      t.string :body
 
       t.timestamps
     end
--- sample_app2/db/schema.rb
+++ sample_app2/db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_06_10_071658) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

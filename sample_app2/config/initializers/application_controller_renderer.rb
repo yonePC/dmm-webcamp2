139d457faa40068a77f967e4771634a66979f5ec
yonePC <ys.mailaddress0929@gmail.com>
2021-06-11 09:17:42 +0000

    Rails2-7

--- sample_app2/config/initializers/application_controller_renderer.rb
+++ sample_app2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '464a688b289118b9aeee0c7efdc2701f1d50f7291d5d4d5bd4cd94cb97502f3f62b7ccfd8472a13be9b7143bea7accb6bef55ba749af863f3213cb21bc15f035'
